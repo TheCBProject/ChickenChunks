54418e5a44bc7299a1d553d9a3ac5302b2448e21
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2022-09-27 17:08:37 -0500

    Added minable by pickaxe tags.

--- src/main/java/codechicken/chunkloader/init/DataGenerators.java
+++ src/main/java/codechicken/chunkloader/init/DataGenerators.java
@@ -1,10 +1,15 @@
 package codechicken.chunkloader.init;
 
+import codechicken.chunkloader.ChickenChunks;
 import codechicken.lib.datagen.LootTableProvider;
 import net.covers1624.quack.util.CrashLock;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by covers1624 on 7/10/20.
@@ -22,6 +27,7 @@ public class DataGenerators {
     private static void gatherDataGenerators(GatherDataEvent event) {
         DataGenerator gen = event.getGenerator();
         gen.addProvider(event.includeServer(), new LootTables(gen));
+        gen.addProvider(event.includeServer(), new BlockTags(gen, event.getExistingFileHelper()));
     }
 
     private static class LootTables extends LootTableProvider.BlockLootProvider {
@@ -39,6 +45,19 @@ public class DataGenerators {
         @Override
         public String getName() {
             return "ChickenChunks Block Loot";
+        }
+    }
+
+    private static class BlockTags extends BlockTagsProvider {
+        public BlockTags(DataGenerator dataGeneratorIn, @Nullable ExistingFileHelper existingFileHelper) {
+            super(dataGeneratorIn, ChickenChunks.MOD_ID, existingFileHelper);
+        }
+
+        @Override
+        protected void addTags() {
+            tag(net.minecraft.tags.BlockTags.MINEABLE_WITH_PICKAXE)
+                    .add(ChickenChunksModContent.CHUNK_LOADER_BLOCK.get())
+                    .add(ChickenChunksModContent.SPOT_LOADER_BLOCK.get());
         }
     }
 }
